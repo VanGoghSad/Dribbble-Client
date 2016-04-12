cb3853eb70fd9dfe1f51c81f8a0f441e1df835fe
VanGoghSad <liwei414833480@gmail.com>
2016-04-12 22:19:56 +0800

    Add TabLayout

--- app/src/main/java/com/example/lw_pc/tribblekill/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/lw_pc/tribblekill/ui/activity/MainActivity.java
@@ -4,18 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.lw_pc.tribblekill.R;
-import com.example.lw_pc.tribblekill.ui.fragment.ListFragment;
+import com.example.lw_pc.tribblekill.ui.fragment.MainFragment;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_details);
+        setContentView(R.layout.activity_main);
 
-       /* getSupportFragmentManager()
+        getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.container, ListFragment.newInstance())
-                .commit();*/
+                .add(R.id.container, MainFragment.newInstance())
+                .commit();
+
+
+
+
     }
 }
